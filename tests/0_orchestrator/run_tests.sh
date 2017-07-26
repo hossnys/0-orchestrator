6e90043a08478c09d6467a26fed0514b3453d07e
john-kheir <kheirj@codescalers.com>
2017-07-26 15:07:29 +0200

    hard code master branch

--- tests/0_orchestrator/run_tests.sh
+++ tests/0_orchestrator/run_tests.sh
@@ -7,7 +7,7 @@ if [ "$TRAVIS_EVENT_TYPE" == "cron" ] || [ "$TRAVIS_EVENT_TYPE" == "api" ]
     then
       pip3 install -r tests/0_orchestrator/test_suite/requirements.txt
       pip3 install git+https://github.com/gigforks/packet-python.git
-      bash tests/0_orchestrator/install_env.sh $TRAVIS_BRANCH $ZT_NET_ID $ZT_TOKEN
+      bash tests/0_orchestrator/install_env.sh master $ZT_NET_ID $ZT_TOKEN
       cd tests/0_orchestrator/; python3 orch_packet_machines.py create $PACKET_TOKEN $ZT_NET_ID $ITSYOUONLINE_ORG $TRAVIS_BRANCH
    elif [ "$point" == "run" ]
     then
